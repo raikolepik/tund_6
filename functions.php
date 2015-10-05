e9135ca42d65f8f1e2b91bfd6494bf3f919a8cab
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-10-05 14:14:25 +0300

    Parandused

--- functions.php
+++ functions.php
@@ -8,35 +8,40 @@
         $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
         
-        $row_nr = 0;
+		// massiiv, kus hoiame autosid
+		$array = array();
 		
-		echo "<table border=5>";
-		echo "<tr><th>rea nr</th><th>auto nr märk</th></tr>"; 
+		
+        // iga rea kohta mis on ab'is teeme midagi
+        while($stmt->fetch()){
+			//suvaline muutuja, kus hoiame auto andmeid selle hetkeni, kui lisame massiivi.
+			
+			
+			// std on tyhi objekt, kus hoiame v22rtusi
+			$car = new StdClass();
+			$car->color = $color_from_db;
+			$car->user_id = $user_id_from_db;
+			$car->id = $id_from_db;
+			$car->number_plate = $number_plate_from_db;
+			//lisan massiivi (auto lisan massiivi)
+			array_push($array, $car);
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre>";
+		}
+        
         // iga rea kohta mis on ab'is teeme midagi
         while($stmt->fetch()){
             //saime andmed kätte
-            echo $row_nr." ".$number_plate_from_db." <br>";
+			//andmed saada transporditavale kujule
 			
-			echo "<tr>";
-			
-			echo "<td>".$row_nr."</td>";
-			echo "<td>".$number_plate_from_db."</td>";
-				
-			echo "</tr>";
 
-		$row_nr++;
         }
-        
-        // iga rea kohta mis on ab'is teeme midagi
-        while($stmt->fetch()){
-            //saime andmed kätte
-			
-			echo $row_nr." ".$number_plate_from_db." <br>";
-			$row_nr++;
-           //echo($user_id_from_db);
-            //? kuidas saada massiivi - SIIT JÄTKAME
-        }
-        
+        //saadan tagasi
+		
+		return $array;
+		
+		
         $stmt->close();
         $mysqli->close();
     }
--- table.php
+++ table.php
@@ -1,6 +1,33 @@
-siia tuleb tabel
-<?php  
+﻿<?php  
     require_once("functions.php");
     
-    getAllData();
+    // kõik autod objektide kujul massiivis
+    $car_array = getAllData();
 ?>
+
+<h1>Tabel</h1>
+<table border = 1>
+<tr>
+	<th>ID</th>
+	<th>Kasutaja ID</th>
+	<th>Auto numbrimärk</th>
+	<th>Värv</th>
+	<th>Kustuta</th>
+	
+</tr>
+<?php 
+    
+    // autod 	ükshaaval läbi käia
+	
+		for($i = 0; $i < count($car_array); $i++){
+			echo "<tr>";
+			echo "<td>".$car_array[$i]->id."</td>";
+			echo "<td>".$car_array[$i]->user_id."</td>";
+			echo "<td>".$car_array[$i]->number_plate."</td>";
+			echo "<td>".$car_array[$i]->color."</td>";
+			echo "<td><a href= '?delete=".$car_array[$i]->id."'>X</a></td>";
+			echo "</tr>";
+    }
+    
+?>
+</table>
