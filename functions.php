8be9a03ed0f4daf5da0a24af9e81bd2876663b43
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-10-05 15:31:40 +0300

    v2ike viga sees

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
     function getAllData(){
         
         $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
         $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
         
@@ -45,6 +45,31 @@
         $stmt->close();
         $mysqli->close();
     }
+    function deleteCarData($car_id){
+		 $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		 //uuendan v2lja deleted
+		 $stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		 $stmt->bind_param("i", $car_id);
+		 $stmt->execute();
+		 
+		 header("location: table.php");
+		 
+		 $stmt->close();
+		 $mysqli->close();
+		 
+	
+		 
+		 
+		
+	}
     
-    
+	
+	    function updateCarData($car_id, $number_plate, $color){
+		 $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		 //uuendan v2lja deleted
+		 $stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
+		 $stmt->bind_param("ssi", $number_plate, $color, $car_id);
+		 $stmt->execute();
+		 $stmr->close();
+	
 ?>
